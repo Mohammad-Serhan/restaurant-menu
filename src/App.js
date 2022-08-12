f838215dc0f429ae2486d91bf04997a1c9557965
Mhamad-Serhan <mohamadserhan07@gmail.com>
2022-08-12 13:09:17 +0200

    install react, tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 // after react v17 , we do not need to use
 // import React from react;
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 // if used several times (Button, Panel, Avatar),
 // or is complex enough on its own (App, FeedStory, Comment) ==> seperate components
 
 function App() {
-  const element = <h1>Hello, world!</h1>; // JSX is a syntax extension to JavaScript
+  // const element = <h1>Hello, world!</h1>; // JSX is a syntax extension to JavaScript
 
   return (
     <div className="App">
-      <header className="content"> {element}</header>
       <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={""}></Route>
+          <Route exact path="/user/register" element={""}></Route>
+          <Route exact path="/user/login" element={""}></Route>
+          {/*  when React starts looking for urls. it looks over all the over routes. but if it does not find it will render NotFound page */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 
-// All React components must act like pure functions with respect to their props.
+// [1] All React components must act like pure functions(does not change inputs) with respect to their props.
 
+// [2] We want to set up a timer whenever the Clock is rendered to the DOM for the first time. This is called “mounting” in React.
+// We also want to clear that timer whenever the DOM produced by the Clock is removed. This is called “unmounting” in React.
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <div className="name">Restaurant</div>
-            <nav className="links">
+        <div className="container flex juxtify-between bg-yellow">
+            <div className="">Restaurant</div>
+            <nav className="">
                 <a href="/" className="">Home</a>
                 <a href="/about" className="">About</a>
 
